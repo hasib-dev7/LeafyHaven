a8bf65f8b118112481b5818f44dee7d83eb8d126
unknown <hasib.dev7@gmail.com>
2025-11-12 01:10:30 +0600

    google signIn add

--- src/AuthenticationPages/Register/Register.jsx
+++ src/AuthenticationPages/Register/Register.jsx
@@ -1,10 +1,12 @@
 import { use } from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { AuthContext } from "../../Context/AuthProvider/AuthContext";
 import { toast } from "react-toastify";
 
 const Register = () => {
-  const { createUser, setUser, updateUserProfile } = use(AuthContext);
+  const { createUser, setUser, updateUserProfile, googleSignIn } =
+    use(AuthContext);
+  const navigate = useNavigate();
   const handleRegister = async (e) => {
     e.preventDefault();
     // get form values
@@ -47,10 +49,12 @@ const Register = () => {
       const signUpUser = res.user;
       updateUserProfile({ displayName: name, photoURL: photo });
       setUser({ ...signUpUser, displayName: name, photoURL: photo });
-      console.log(signUpUser)
+      console.log(signUpUser);
       // form reset
       e.target.reset();
       toast.success(`Welcome, ${name}! Your account has been created.`);
+      // path  navigate to home page
+      navigate("/");
     } catch (err) {
       // form reset
       e.target.reset();
@@ -66,6 +70,14 @@ const Register = () => {
       }
       console.log("Registration error:", err.code);
     }
+  };
+  // google login
+  const handleGoogle = () => {
+    googleSignIn().then((res) => {
+      setUser(res.user);
+      // path  navigate to home page
+      navigate("/");
+    });
   };
   return (
     <>
@@ -112,6 +124,40 @@ const Register = () => {
                 />
 
                 <button className="btn btn-neutral mt-4">Register</button>
+                {/* Google */}
+                <button
+                  onClick={handleGoogle}
+                  className="btn bg-white text-black border-[#e5e5e5]"
+                >
+                  <svg
+                    aria-label="Google logo"
+                    width="16"
+                    height="16"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <g>
+                      <path d="m0 0H512V512H0" fill="#fff"></path>
+                      <path
+                        fill="#34a853"
+                        d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                      ></path>
+                      <path
+                        fill="#4285f4"
+                        d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                      ></path>
+                      <path
+                        fill="#fbbc02"
+                        d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                      ></path>
+                      <path
+                        fill="#ea4335"
+                        d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                      ></path>
+                    </g>
+                  </svg>
+                  Login with Google
+                </button>
               </fieldset>
               <p>
                 Have youer already account ? please{" "}
